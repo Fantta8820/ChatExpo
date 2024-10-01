f012bef09febf123f0c64dcc570ca70eee57f904
Fantta8820 <andrevictor2512@gmail.com>
2024-10-01 11:13:57 -0300

    01/10

--- app/signIn.js
+++ app/signIn.js
@@ -21,33 +21,79 @@ import CustomKeyboardView from '../components/CustomKeyboardView';
 import { useAuth } from '../context/authContext';
 
 export default function SignIn() {
-    const router = useRouter();
+  const router = useRouter();
 
-    const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-    const { login } = useAuth();
+  const { login } = useAuth();
 
-    const emailRef = useRef('');
-    const passwordRef = useRef('');
+  const emailRef = useRef('');
+  const passwordRef = useRef('');
 
-    const handleLogin = async () => {   
-        if (!emailRef.current || !passwordRef.current) {
-            Alert.alert('Sign In', 'Por favor, preencha todos os campos');
-        }
-    };
+  const handleLogin = async () => {
+    if (!emailRef.current || !passwordRef.current) {
+      Alert.alert('Sign In', 'Por favor, preencha todos os campos');
+    }
+    setLoading(true);
+    const response = await login(emailRef.current, passwordRef.current);
+    setLoading(false);
 
-    return (
-        <CustomKeyboardView>
-            <StatusBar style='dark' />
-            <View style={{ paddingTop: hp(8), paddingHorizontal: wp(5) }} className='flex-1 gap-12'>
-                <View className='items-center'>
-                    <Image style={{ height: hp(25) }} resizeMode='contain' source={require('../assets/images/login.png')} />
-                </View>
+    if (!response.success) {
+      Alert.alert('Sign In', response.msg);
+    }
+  };
 
-                <View className='gap-10'>
-                    <Text style={ { fontSize: hp(4) } } className='font-bold tracking-wider text-center text-neutral-800'>Sign In</Text>
-                </View>
+  return (
+    <CustomKeyboardView>
+      <StatusBar style="dark" />
+      <View style={{ paddingTop: hp(8), paddingHorizontal: wp(5) }} className="flex-1 gap-12">
+        <View className="items-center">
+          <Image
+            style={{ height: hp(25) }}
+            resizeMode="contain"
+            source={require('../assets/images/login.png')}
+          />
+        </View>
+
+        <View className="gap-10">
+          <Text
+            style={{ fontSize: hp(4) }}
+            className="font-bold tracking-wider text-center text-neutral-800">
+            Sign In
+          </Text>
+
+          <View className="gap-10">
+            <View
+              style={{ height: hp(7) }}
+              className="flex-row gap-4 px-4 bg-neutral-100 items-center rounded-xl">
+              <Octicons name="mail" size={{ fontSize: hp(2.7) }} color="gray" />
+              <TextInput
+                onChangeText={(value) => (emailRef.current = value)}
+                style={{ fontSize: hp(2) }}
+                className="flex-1 font-semibold text-neutral-700"
+                placeholder="Email address"
+                placeholderTextColor={'gray'}
+              />
             </View>
-        </CustomKeyboardView>
-    );
+
+            <View
+              style={{ height: hp(7) }}
+              className="flex-row gap-4 px-4 bg-neutral-100 items-center rounded-xl">
+              <Octicons name="lock" size={{ fontSize: hp(2.7) }} color="gray" />
+              <TextInput
+                onChangeText={(value) => (emailRef.current = value)}
+                style={{ fontSize: hp(2) }}
+                className="flex-1 font-semibold text-neutral-700"
+                placeholder="Password"
+                secureTextEntry
+                placeholderTextColor={'gray'}
+              />
+            </View>
+
+            <Text style={{fontSize: hp(1.8)}} className="font-semibold text-right text-neutral-500"></Text>
+          </View>
+        </View>
+      </View>
+    </CustomKeyboardView>
+  );
 }
